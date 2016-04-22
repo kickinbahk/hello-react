6b53a45201899b258d1ad202b0bfdbd2e03541e2
kickinbahk <kickinbahk@gmail.com>
2016-04-21 18:28:06 -0700

    add form and state for input

--- public/app.jsx
+++ public/app.jsx
@@ -1,28 +1,47 @@
 /* global React, ReactDOM */
 
-var Greeter = React.createClass({ // eslint-disable-line no-use-before-define
+var Greeter = React.createClass({
   getDefaultProps: function () {
     return {
       name: 'React',
       message: 'This is from a component'
     }
   },
+  getInitialState: function () {
+    return {
+      name: this.props.name
+    }
+  },
+  onButtonClick: function (e) {
+    e.preventDefault()
+
+    var name = this.refs.name.value
+
+    this.setState({
+      name: name
+    })
+  },
   render: function () {
-    var name = this.props.name
-    var givenMessage = this.props.message
+    var name = this.state.name
+    var message = this.props.message
     return (
       <div>
         <h1>Hello, {name}!</h1>
-        <p>{givenMessage}</p>
+        <p>{message}</p>
+
+        <form onSubmit={this.onButtonClick}>
+          <input type='text' ref='name' />
+          <button>Set Name</button>
+        </form>
       </div>
     )
   }
 })
 
 var firstName = 'World'
-var message = 'Welcome Home...'
+var setMessage = 'This is the paragraph'
 
 ReactDOM.render(
-  <Greeter name={firstName} message={message} />,
+  <Greeter name={firstName} message={setMessage} />,
   document.getElementById('app')
 )
