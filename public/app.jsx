e4b04e72bf7ddcd1bbb1b03c04975fb0077148ab
kickinbahk <kickinbahk@gmail.com>
2016-04-22 08:42:17 -0700

    add functionality to updete message

--- public/app.jsx
+++ public/app.jsx
@@ -16,19 +16,35 @@ var GreeterForm = React.createClass({
   onFormSubmit: function (e) {
     e.preventDefault()
 
+    var updates = {}
     var name = this.refs.name.value
+    var message = this.refs.message.value
 
     if (name.length > 0) {
       this.refs.name.value = ''
-      this.props.onNewName(name)
+      updates.name = name
     }
+
+    if (message.length > 0) {
+      this.refs.message.value = ''
+      updates.message = message
+    }
+
+    this.props.onNewData(updates)
   },
   render: function () {
     return (
       <div>
         <form onSubmit={this.onFormSubmit}>
-          <input type='text' ref='name' />
-          <button>Set Name</button>
+          <div>
+            <input type='text' ref='name' placeholder='Enter Name'/>
+          </div>
+          <div>
+            <textarea ref='message' placeholder='Enter Message'></textarea>
+          </div>
+          <div>
+            <button>Submit</button>
+          </div>
         </form>
       </div>
     )
@@ -39,33 +55,31 @@ var Greeter = React.createClass({
   getDefaultProps: function () {
     return {
       name: 'React',
-      message: 'This is from a component'
+      message: 'I update without reloading'
     }
   },
   getInitialState: function () {
     return {
-      name: this.props.name
+      name: this.props.name,
+      message: this.props.message
     }
   },
-  handleNewName: function (name) {
-    this.setState({
-      name: name
-    })
+  handleNewData: function (updates) {
+    this.setState(updates)
   },
   render: function () {
     var name = this.state.name
-    var message = this.props.message
+    var message = this.state.message
     return (
       <div>
         <GreeterMessage name={name} message={message}/>
-        <GreeterForm onNewName={this.handleNewName}/>
+        <GreeterForm onNewData={this.handleNewData}/>
       </div>
     )
   }
 })
 
-var firstName = 'World'
 ReactDOM.render(
-  <Greeter name={firstName} />,
+  <Greeter />,
   document.getElementById('app')
 )
