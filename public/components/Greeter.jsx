dc5f6630b4528b6274da3b875e695cf501976994
kickinbahk <kickinbahk@gmail.com>
2016-04-30 13:17:21 -0700

    move app code to components

--- public/components/Greeter.jsx
+++ public/components/Greeter.jsx
@@ -1,5 +1,33 @@
-function greeter () {
-  document.write('From Greeter function')
-}
+var React = require('react')
+var GreeterMessage = require('./GreeterMessage')
+var GreeterForm = require('./GreeterForm')
 
-module.exports = greeter
+var Greeter = React.createClass({
+  getDefaultProps: function () {
+    return {
+      name: 'React',
+      message: 'I update without reloading'
+    }
+  },
+  getInitialState: function () {
+    return {
+      name: this.props.name,
+      message: this.props.message
+    }
+  },
+  handleNewData: function (updates) {
+    this.setState(updates)
+  },
+  render: function () {
+    var name = this.state.name
+    var message = this.state.message
+    return (
+      <div>
+        <GreeterMessage name={name} message={message}/>
+        <GreeterForm onNewData={this.handleNewData}/>
+      </div>
+    )
+  }
+})
+
+module.exports = Greeter
